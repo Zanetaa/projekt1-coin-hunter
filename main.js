57efeeab11038df6a8fc0451018ac8d18a447752
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-03-05 15:17:22 +0100

    Náhodný pohyb mince - vygoogleno

--- main.js
+++ main.js
@@ -10,17 +10,30 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 // Panáček do prostřed stránky
 
-function priNacteniStranky() {
-	
+/*function priNacteniStranky() {*/
+
 let panacek= document.getElementById('panacek');
 panacek.style.left= '500px';
 panacek.style.top= '260px';
 
+/*}*/
+
+//Náhodné vygenerování mince /vygoogleno: http://www.mendoweb.be/blog/javascript-random-element-position/
+
+function getRandomPosition(element) {
+	var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;
+	var randomX = Math.floor(Math.random()*x);
+	var randomY = Math.floor(Math.random()*y);
+	return [randomX,randomY];
 }
 
-//Náhodné vygenerování mince
-
-
-
-
-
+window.onload = function() {
+	var img = document.createElement('img');
+	img.setAttribute("style", "position:absolute;");
+	img.setAttribute("src", "obrazky/mince.png");
+	document.body.appendChild(img);
+	var xy = getRandomPosition(img);
+	img.style.top = xy[0] + 'px';
+	img.style.left = xy[1] + 'px';
+}
